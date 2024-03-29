9f8c94d2d872092060f5f2fa46c258c97cdafc6b
togrulagayev <togrullagayev@gmail.com>
2024-03-29 17:31:58 +0400

    update

--- f_lesson_12/lib/main.dart
+++ f_lesson_12/lib/main.dart
@@ -11,31 +11,47 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Stack Widget Example'),
-        ),
-        body: Stack(
-          children: [
-            // ! Align widget vidgetlerin movqeyini teyin edir
-            const CircleAvatar(
-              foregroundImage: NetworkImage(
-                  'https://icones.pro/wp-content/uploads/2021/03/avatar-de-personne-icone-homme.png'),
-              radius: 100,
+          appBar: AppBar(
+            title: const Text(
+              'Stack Widget Example',
+              style: TextStyle(
+                fontFamily: 'Josefin Sans',
+              ),
             ),
-            Align(
-              alignment: const Alignment(-0.15, -0.6),
-              child: IconButton(
-                onPressed: () {},
-                icon: SvgPicture.asset(
-                  'assets/svgs/add_circle.svg',
-                  // width: 50,
-                  height: 60,
+            centerTitle: true,
+          ),
+          body: ListView(
+            children: [
+              Expanded(
+                child: Stack(
+                  children: [
+                    // ! Align widget vidgetlerin movqeyini teyin edir
+                    const CircleAvatar(
+                      foregroundImage: NetworkImage(
+                          'https://icones.pro/wp-content/uploads/2021/03/avatar-de-personne-icone-homme.png'),
+                      radius: 100,
+                    ),
+                    Align(
+                      alignment: const Alignment(-0.15, -0.6),
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: SvgPicture.asset(
+                          'assets/svgs/add_circle.svg',
+                          // width: 50,
+                          height: 60,
+                        ),
+                      ),
+                    )
+                  ],
                 ),
               ),
-            )
-          ],
-        ),
-      ),
+              Image.asset('assets/images/one.webp'),
+              Image.asset('assets/images/two.webp'),
+              Image.asset('assets/images/three.jpg'),
+              Image.asset('assets/images/four.webp'),
+              Image.asset('assets/images/five.webp'),
+            ],
+          )),
     );
   }
 }
